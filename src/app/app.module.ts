c30c4f421ba32209f6a446427cfb48203c7193bb
Giovanni Vargas <jonan-vargas23@hotmail.com>
2021-03-07 16:28:32 -0500

    Se agrega nuevo componente tarjeta-credito y se agrega gradiente en el fondo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TarjetaCreditoComponent } from './components/tarjeta-credito/tarjeta-credito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarjetaCreditoComponent
   ],
   imports: [
     BrowserModule,
